a13ea5a40807730f5ef3e9d70beb1339850720a1
Hackers In Sri Lanka <113082337+hackersinsrilankaofc@users.noreply.github.com>
2025-07-31 13:38:12 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~Y6UlnaxD#tCpRdunM97G3qyFDjD7YTzhyzocxXeNDddC238UV6zc",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "94779529221", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
